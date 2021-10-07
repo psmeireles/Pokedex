6d9a744078788192703d8674003e1c91a4156dff
Pedro Sousa Meireles <psmeireles25@gmail.com>
2021-10-07 20:18:32 -0300

    Create Pokemon and Type entities

--- Pokedex/Data/Pokemon.cs
+++ Pokedex/Data/Pokemon.cs
@@ -2,6 +2,18 @@
 {
     public class Pokemon
     {
-        
+        public int Id;
+        public string Name;
+        public Type Type1;
+        public Type Type2;
+        public int Total;
+        public int HP;
+        public int Attack;
+        public int Defense;
+        public int SpAtk;
+        public int SpDef;
+        public int Speed;
+        public int Generation;
+        public bool Legendary;
     }
 }
---
+++ Pokedex/Data/Type.cs
@@ -0,0 +1,24 @@
+﻿namespace Pokedex.Data
+{
+    public enum Type
+    {
+        Normal,
+        Fighting,
+        Flying,
+        Poison,
+        Ground,
+        Rock,
+        Bug,
+        Ghost,
+        Steel,
+        Fire,
+        Water,
+        Grass,
+        Electric,
+        Psychic,
+        Ice,
+        Dragon,
+        Dark,
+        Fairy
+    }
+}

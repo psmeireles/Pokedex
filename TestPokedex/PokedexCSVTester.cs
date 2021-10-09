0a13250ecdd12e72f3a4280f13fd3c38df8e1fa5
Pedro Sousa Meireles <psmeireles25@gmail.com>
2021-10-08 23:13:37 -0300

    GetById_Success test

--- TestPokedex/PokedexCSVTester.cs
+++ TestPokedex/PokedexCSVTester.cs
@@ -17,6 +17,17 @@ namespace TestPokedex
             Assert.AreEqual(_repository.GetCount(), 800);
         }
 
+        [DataRow(1)]
+        [DataRow(10)]
+        [DataRow(100)]
+        [DataRow(721)]
+        [DataTestMethod]
+        public void GetById_Success(int id)
+        {
+            var uintId = (uint) id;
+            var pokemon = _repository.GetById(uintId);
+            Assert.AreEqual(pokemon.Id, uintId);
+        }
         [TestMethod]
         public void TestMethod1()
         {
